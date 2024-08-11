d1877937aac3b820f0ab04cce820db5329685d07
Terrarich <163340619+terrarich@users.noreply.github.com>
2024-08-11 16:54:43 +0300

    discription and tips

--- main.cpp
+++ main.cpp
@@ -3,7 +3,9 @@ using namespace std;
 
 void main()
 {
-	const int DAYS = 7;
-	const char* TAB = "\t";
+	const int DAYS = 7; //Константы в следующих строках НЕЛЬЗЯ изменить | Constants in the following lines CANNOT be changed
+	const char* TAB = "\t"; 
 	cout << DAYS << TAB;
 }
+
+// Константы лучше именовать большими буквами | It is better to name constants in capital letters
